499e600118e724c6093f3f98e6facede8f893bef
Leon <leon.wehinger@htldornbirn.at>
2025-02-27 08:46:01 +0100

    Games 01 finished

--- src/at/wle/games/firstGame/Rectangles.java
+++ src/at/wle/games/firstGame/Rectangles.java
@@ -1,27 +1,64 @@
 package at.wle.games.firstGame;
 
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
+import org.newdawn.slick.tests.AnimationTest;
 
 public class Rectangles extends BasicGame {
+
+    private int x;
+    private int y;
+
+
+
     public Rectangles(String title) {
         super(title);
     }
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
+        this.x=0;
+        this.y=0;
+
+
 
     }
 
     @Override
     public void update(GameContainer gameContainer, int i) throws SlickException {
+        this.x++;
+        this.y++;
+
+        if (this.x>800){
+            this.x=0;
+            this.y=0;
+        }
+
+
 
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
 
+        graphics.setColor(Color.white);
+        graphics.drawRect(200,100,50,100);
+        graphics.drawRect(500,100,50,100);
+        graphics.drawString("Hi Nejolein!",this.x,this.y);
+        graphics.setColor(Color.blue);
+        graphics.fillRect(150,300,450,50);
+
+
     }
+
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new Rectangles("Rectangles"));
+            container.setDisplayMode(800,600,false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
